07e362492ca974e8d3f7ad1dbbb2e94b110a6a48
enoch wu <enoch.wu@kingsway.school.nz>
2021-11-09 14:22:36 +1300

    cookies added

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+// Set a Cookie
+function setCookie(cName, cValue, expDays) {
+    let date = new Date();
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
+}
+function getCookie(cName) {
+    const name = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie); //to be careful
+    const cArr = cDecoded.split('; ');
+    let res;
+    cArr.forEach(val => {
+        if (val.indexOf(name) === 0) res = val.substring(name.length);
+    })
+    return res;
+}
+
 $(document).ready(function () {
     if (Notification.permission !== "granted") {
         Notification.requestPermission();
@@ -7,3 +26,21 @@ $(document).ready(function () {
     
     
 })
+function getNextCookieValue() {
+    var cookieValue
+    for (var i = 1; getCookie(i) != "undefined"; i++) {
+        cookieValue = i;
+    }
+    cookieValue++;
+    return cookieValue;
+}
+function newTime() {
+    var date = $("#date").val();
+    console.log(date);
+    var time = $("#time").val();
+    console.log(time);
+    var name = $("#name").val();
+    console.log(name);
+    var newCookie = getNextCookieValue();
+    setCookie(newCookie, date + "/" + time + "/" + name + "/", 365);
+}
